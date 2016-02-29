4899ff0021bb74e89d0b9bf305ed239418d5c094
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2016-02-29 21:18:20 +0100

    Scaffolding

---
+++ DotNetDllApiDump/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DotNetDllApiDump")]
+[assembly: AssemblyDescription("Textual dump the public Api of a DLL to help you when using ILMerge.exe")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Kasper Graversen inc.")]
+[assembly: AssemblyProduct("DotNetDllApiDump")]
+[assembly: AssemblyCopyright("Copyright ©  2016 Kasper B. Graversen")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("415fb8ad-75bd-45af-b2f7-79807159ee47")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
